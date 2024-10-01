bcd82baf0ce0b753d29ef4f17bedbc60a7875511
unknowndeals <161517463+unknowndeals@users.noreply.github.com>
2024-10-01 16:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "Jai Shree Ram."
 
+
 if __name__ == "__main__":
-    # Get the PORT from the environment, default to 8000 if not set
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)  # Host should be "0.0.0.0"
+    app.run()
 
